5b745fbb746321e3d89027bb327ade1e850a59fd
jieun Kang <rjeh321@naver.com>
2023-11-22 12:31:12 +0900

    firebase

--- resources/firebase.js
+++ resources/firebase.js
@@ -1,11 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
  import{
-  getFirestore, 
-  collection, 
+  getFirestore,
+  collection,
   getDocs,
+  getDoc,
   setDoc,
   addDoc,
   doc,
+  deleteDoc,
+  updateDoc,
+  deleteField,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js"
  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
  
@@ -18,16 +22,49 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebas
    appId: "1:100957826346:web:3e61f17bd09bad4cb08253",
    measurementId: "G-2F7T0J10W2"
  };
- const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
- const  db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-
- async function getDatas(collectionName){
+async function getDatas(collectionName) {
   const querySnapshot = await getDocs(collection(db, collectionName));
-  return querySnapshot
+  return querySnapshot;
 }
-async function addDatas(collection, dataObj){
-  await addDoc(collection(db,collectionName), dataObj);
+
+async function addDatas(collectionName, dataObj) {
+  // 문서ID 부여
+  //   await setDoc(doc(db, "member", "member1"), dataObj);
+
+  // 문서ID 자동
+  await addDoc(collection(db, collectionName), dataObj);
 }
- export { db,getDocs,collection,getDatas,setDoc,addDoc,doc,addDatas };
+
+async function deleteDatas(collectionName, docId) {
+  await deleteDoc(doc(db, collectionName, docId));
+}
+
+async function updateDatas(collectionName, docId, updateInfoObj) {
+  const docRef = await doc(db, collectionName, docId);
+  const docData = await getDoc(docRef);
+  console.log(docData);
+  debugger;
+  // 문서 필드 데이터 수정
+  await updateDoc(docRef, updateInfoObj);
+}
+
+export {
+  db,
+  getDocs,
+  collection,
+  getDatas,
+  setDoc,
+  addDoc,
+  doc,
+  addDatas,
+  deleteDoc,
+  deleteDatas,
+  updateDoc,
+  deleteField,
+  updateDatas,
+};
